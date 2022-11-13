3838a2e86cfd8975e3dab822a46acc9202974c59
Aryankrgupta <aryankumargupta1106@gmail.com>
2022-11-13 13:50:09 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider, theme } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
+      <Analytics />
     </ChakraProvider>
   </React.StrictMode>
 );
